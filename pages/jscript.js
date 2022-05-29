c5f042a34de21ccafbbe543624ffb9f0bb2e2b45
Eugene Popov <popovich174@yandex.ru>
2022-05-29 19:45:57 +0300

    Исправлено согласно второму ревью ао JS

--- pages/jscript.js
+++ pages/jscript.js
@@ -7,22 +7,21 @@ const addButton = document.querySelector(".profile__button-add");
 const gallery = document.querySelector(".gallery");
 const formEditProfile = document.querySelector(".popup__form_place_edit-profile");
 const formNewPlace = document.querySelector(".popup__form_place_new-place");
-
-let popupEditProfile = document.querySelector(".popup_place_edit-profile");
-let popupNewPlace = document.querySelector(".popup_place_new-place");
-let popupPhoto = document.querySelector(".popup_place_photo");
-let inputName = document.querySelector(".popup__input_el_name");
-let inputAbout = document.querySelector(".popup__input_el_about");
-let inputPlace = document.querySelector(".popup__input_el_place");
-let inputUrl = document.querySelector(".popup__input_el_url");
-let profileName = document.querySelector(".profile__name");
-let profileAbout = document.querySelector(".profile__about");
-let gallerySubtitle = document.querySelector(".gallery__subtitle");
-let galleryPhoto = document.querySelector("gallery__photo");
-let popupSubtitle = document.querySelector(".popup__subtitle");
-let popupImage = document.querySelector(".popup__image");
-
-let initialCards = [
+const cardTemplate = document.querySelector(".gallery__template").content;
+const popupEditProfile = document.querySelector(".popup_place_edit-profile");
+const popupNewPlace = document.querySelector(".popup_place_new-place");
+const popupPhoto = document.querySelector(".popup_place_photo");
+const inputName = document.querySelector(".popup__input_el_name");
+const inputAbout = document.querySelector(".popup__input_el_about");
+const inputPlace = document.querySelector(".popup__input_el_place");
+const inputUrl = document.querySelector(".popup__input_el_url");
+const profileName = document.querySelector(".profile__name");
+const profileAbout = document.querySelector(".profile__about");
+const gallerySubtitle = document.querySelector(".gallery__subtitle");
+const galleryPhoto = document.querySelector("gallery__photo");
+const popupSubtitle = document.querySelector(".popup__subtitle");
+const popupImage = document.querySelector(".popup__image");
+const initialCards = [
   {
     name: "Архыз",
     link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
@@ -71,90 +70,62 @@ function formSubmitProfile(event) {
   closePopup(popupEditProfile);
 }
 
-// Функция добавления стартовых карточек
+// Функция добавления карточки
 
-function addInitialCards(card, gallery) {
-  gallery.append(card);
+function addCard(card, gallery) {
+  gallery.prepend(card);
 }
 
 // Функция создания стартовых карточек
 
-function createInitialCards(initialCards) {
-  for (i = 0; i < initialCards.length; i = i + 1) {
-    let cardTemplate = document.querySelector(".gallery__template").content;
-    let card = cardTemplate.querySelector(".gallery__card").cloneNode(true);
-    let cardPhoto = card.querySelector(".gallery__photo");
-    let cardSubtitle = card.querySelector(".gallery__subtitle");
+function createCards(obj) {
 
-    cardPhoto.setAttribute("src", initialCards[i].link);
-    cardPhoto.setAttribute("alt", initialCards[i].name);
-    cardSubtitle.textContent = initialCards[i].name;
+  obj.forEach(function(item) {
+
+    const card = cardTemplate.querySelector(".gallery__card").cloneNode(true);
+    const cardPhoto = card.querySelector(".gallery__photo");
+    const cardSubtitle = card.querySelector(".gallery__subtitle");
+
+    cardPhoto.setAttribute("src", item.link);
+    cardPhoto.setAttribute("alt", item.name);
+    cardSubtitle.textContent = item.name;
 
     card.querySelector(".gallery__like").addEventListener("click", function (event) {
-        event.target.classList.toggle("gallery__like_active");
-      });
+      event.target.classList.toggle("gallery__like_active");
+    });
 
     card.querySelector(".gallery__delete").addEventListener("click", function (event) {
-        event.target.closest(".gallery__card").remove();
-      });
+      event.target.closest(".gallery__card").remove();
+    });
 
     cardPhoto.addEventListener("click", function () {
-      popupPhoto.classList.add("popup_opened");
+      openPopup(popupPhoto);
       popupSubtitle.textContent = cardSubtitle.textContent;
       popupImage.setAttribute("src", cardPhoto.getAttribute("src"));
       popupImage.setAttribute("alt", cardPhoto.getAttribute("alt"));
     });
 
-    addInitialCards(card, gallery);
-  }
+    addCard(card, gallery);
+
+  })
+
 }
 
-createInitialCards(initialCards);
-
-// Функция создания карточки
-
-function createCard(inputPlaceValue, inputUrlValue) {
-  let cardTemplate = document.querySelector(".gallery__template").content;
-  let card = cardTemplate.querySelector(".gallery__card").cloneNode(true);
-  let cardPhoto = card.querySelector(".gallery__photo");
-  let cardSubtitle = card.querySelector(".gallery__subtitle");
-
-  cardSubtitle.textContent = inputPlaceValue;
-  cardPhoto.setAttribute("src", inputUrlValue);
-  cardPhoto.setAttribute("alt", inputPlaceValue);
-
-  card.querySelector(".gallery__like").addEventListener("click", function (event) {
-      event.target.classList.toggle("gallery__like_active");
-    });
-
-  card.querySelector(".gallery__delete").addEventListener("click", function (event) {
-      event.target.closest(".gallery__card").remove();
-    });
-
-  cardPhoto.addEventListener("click", function () {
-    popupPhoto.classList.add("popup_opened");
-    popupSubtitle.textContent = cardSubtitle.textContent;
-    popupImage.setAttribute("src", cardPhoto.getAttribute("src"));
-    popupImage.setAttribute("alt", cardPhoto.getAttribute("alt"));
-  });
-
-  return card;
-}
-
-// Промежуточная функция добавления карточки
-
-function addCard(card, gallery) {
-  gallery.prepend(card);
-}
+createCards(initialCards);
 
 // Функция добавления карточки
 
 function formSubmitPlace(event) {
   event.preventDefault();
 
-  createCard(inputPlace.value, inputUrl.value);
+  const arr = [
+    {
+    name: inputPlace.value,
+    link: inputUrl.value,
+    },
+  ]
 
-  addCard(createCard(inputPlace.value, inputUrl.value), gallery);
+  createCards(arr);
 
   closePopup(popupNewPlace);
 
