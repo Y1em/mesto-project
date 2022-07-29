95152557d85041546a51501c8ede8d440521dc67
Paul Flesh <46377053+PaulFlesh@users.noreply.github.com>
2022-07-29 14:25:48 +0400

    Popup.js правки

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -3,23 +3,33 @@ export default class Popup {
     this._popup = document.querySelector(popupSelector);
   }
 
-  _handleEscClose = (evt) => {
+  _handleEscClose(evt) {
     if (evt.key === 'Escape') {
       this.close();
     }
   }
 
+  _handlecloseByOverlay(evt) {
+    if (evt.target.classList.contains('popup_opened')) {
+      this.close();
+    }
+  };
+
   open() {
     this._popup.classList.add('popup_opened');
-    document.addEventListener('keydown', this._handleEscClose);
+    document.addEventListener('keydown', () => this._handleEscClose(evt));
+    document.addEventListener('mousedown', () => this._handlecloseByOverlay(evt));
   }
 
   close() {
     this._popup.classList.remove('popup_opened');
     document.removeEventListener('keydown', this._handleEscClose);
+    document.removeEventListener('mousedown', this._handlecloseByOverlay);
   }
 
   setEventListeners() {
+    this._popup.querySelector('.popup__button-close').addEventListener('click', () => this.close());
+    /*
     this._popup.addEventListener('mousedown', (evt) => {
       if (evt.target.classList.contains('popup__button-close') ||
           evt.target.classList.contains('popup') ||
@@ -27,6 +37,7 @@ export default class Popup {
             this.close();
       }
     });
+    */
   }
   returnPopup() {
     return this._popup;
